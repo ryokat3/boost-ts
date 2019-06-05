1c23aef3c23dc0c16a2a2d9b335f92188a297397
Ryoji Kato <ryoji.kato@ericsson.com>
2019-06-05 18:12:20 +0900

    Added 'readerify' for Reader monad

--- src/partial.ts
+++ src/partial.ts
@@ -1,5 +1,6 @@
-import { Top, Zip, Unzip2nd, Select, SelectMask, MapUnion, Cast } from "./tuplelib"
+import { Length, Top, Zip, Unzip2nd, Select, SelectMask, MapUnion, Cast } from "./tuplelib"
 import { ArgumentsType } from "./functionlib"
+import { Comp } from "./numberlib"
 
 /**
  * partial
@@ -25,31 +26,58 @@ type PHMaskArgs<Args extends any[]> = SelectMask<PH_TYPE, Args> extends infer X1
 type ZipFuncPH<FuncArgs extends any[], Args extends any[]> = Zip<PHMaskArgs<Args>, FuncArgs> extends infer X2 ? Cast<X2,any[][]> : never
 type SelectedZipFuncPH<FuncArgs extends any[], Args extends any[]> = Select<[PH_TYPE, unknown], ZipFuncPH<FuncArgs, Args>> extends infer X3 ? Cast<X3,any[][]> : never
 type SortedZipFuncPH<FuncArgs extends any[], Args extends any[]> = SortZipByPH<SelectedZipFuncPH<FuncArgs, Args> extends infer X4 ? Cast<X4,any[][]> : never>
-type PartialArgs<FuncArgs extends any[], Args extends any[]> = Unzip2nd<SortedZipFuncPH<FuncArgs,Args> extends infer X5 ? Cast<X5,any[][]>: never> extends infer X6 ? Cast<X6, any[]> : never
 
 
-/*
-export function partial<A1, R, X1 extends A1|PH_TYPE >(func:(a1:A1)=>R, x1:X1):(...args:PartialArgs<[A1],[X1]>)=>R
-export function partial<A1, A2, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE>(func:(a1:A1, a2:A2)=>R, x1:X1, x2:X2):(...args:PartialArgs<[A1,A2],[X1,X2]>)=>R
-export function partial<A1, A2, A3, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE, X3 extends A3|PH_TYPE>(func:(a1:A1, a2:A2, a3:A3)=>R, x1:X1, x2:X2, x3:X3):(...args:PartialArgs<[A1,A2,A3],[X1,X2,X3]>)=>R
-export function partial<A1, A2, A3, A4, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE, X3 extends A3|PH_TYPE, X4 extends A4|PH_TYPE>(func:(a1:A1, a2:A2, a3:A3, a4:A4)=>R, x1:X1, x2:X2, x3:X3, x4:X4):(...args:PartialArgs<[A1,A2,A3,A4],[X1,X2,X3,X4]>)=>R
-export function partial<A1, A2, A3, A4, A5, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE, X3 extends A3|PH_TYPE, X4 extends A4|PH_TYPE, X5 extends A5|PH_TYPE>(func:(a1:A1, a2:A2, a3:A3, a4:A4, a5:A5)=>R, x1:X1, x2:X2, x3:X3, x4:X4, x5:X5):(...args:PartialArgs<[A1,A2,A3,A4,A5],[X1,X2,X3,X4,X5]>)=>R
-export function partial<A1, A2, A3, A4, A5, A6, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE, X3 extends A3|PH_TYPE, X4 extends A4|PH_TYPE, X5 extends A5|PH_TYPE, X6 extends A6|PH_TYPE>(func:(a1:A1, a2:A2, a3:A3, a4:A4, a5:A5, a6:A6)=>R, x1:X1, x2:X2, x3:X3, x4:X4, x5:X5, x6:X6):(...args:PartialArgs<[A1,A2,A3,A4,A5,A6],[X1,X2,X3,X4,X5,X6]>)=>R
-export function partial<A1, A2, A3, A4, A5, A6, A7, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE, X3 extends A3|PH_TYPE, X4 extends A4|PH_TYPE, X5 extends A5|PH_TYPE, X6 extends A6|PH_TYPE, X7 extends A7|PH_TYPE>(func:(a1:A1, a2:A2, a3:A3, a4:A4, a5:A5, a6:A6, a7:A7)=>R, x1:X1, x2:X2, x3:X3, x4:X4, x5:X5, x6:X6, x7:X7):(...args:PartialArgs<[A1,A2,A3,A4,A5,A6,A7],[X1,X2,X3,X4,X5,X6,X7]>)=>R
-export function partial<A1, A2, A3, A4, A5, A6, A7, A8, R, X1 extends A1|PH_TYPE , X2 extends A2|PH_TYPE, X3 extends A3|PH_TYPE, X4 extends A4|PH_TYPE, X5 extends A5|PH_TYPE, X6 extends A6|PH_TYPE, X7 extends A7|PH_TYPE, X8 extends A8|PH_TYPE>(func:(a1:A1, a2:A2, a3:A3, a4:A4, a5:A5, a6:A6, a7:A7, a8:A8)=>R, x1:X1, x2:X2, x3:X3, x4:X4, x5:X5, x6:X6, x7:X7, x8:X8):(...args:PartialArgs<[A1,A2,A3,A4,A5,A6,A7,A8],[X1,X2,X3,X4,X5,X6,X7,X8]>)=>R
+export type PartialUnboundArgsType<FuncArgs extends any[], Args extends any[]> = Unzip2nd<SortedZipFuncPH<FuncArgs,Args> extends infer X5 ? Cast<X5,any[][]>: never> extends infer X6 ? Cast<X6, any[]> : never
+export type PartialArgsType<Func extends (...args:any[])=>any> = MapUnion<PH_TYPE, ArgumentsType<Func> extends infer X1 ? Cast<X1, any[]> : never> extends infer X2 ? Cast<X2, any[]> : never
 
-export function partial<R>(func:(...args:any[])=>R, ...argsOrig:any[]):(...args:any[])=>R {
-    return function (...partialArgs:any[]) {
-       return func.call(func, ...partialArgs.reduce((result:any[], arg:any, idx:number)=>result.map((x:any)=>(x === PH_LIST[idx] ? arg : x)), argsOrig))
-    }
-}
-*/
-
-
-type ActualArgsType<Func extends (...args:any[])=>any> = MapUnion<PH_TYPE, ArgumentsType<Func> extends infer X1 ? Cast<X1, any[]> : never> extends infer X2 ? Cast<X2, any[]> : never
-
-export function partial<Func extends (...args:any[])=>any, Args extends ActualArgsType<Func>>(func:Func, ...argsOrig:Args):(...partialArgs:PartialArgs<ArgumentsType<Func>, Args>)=>ReturnType<Func> {
-    return function (...partialArgs:any[]) {    
-        return func.call(func, ...partialArgs.reduce((result:any[], arg:any, idx:number)=>result.map((x:any)=>(x === PH_LIST[idx] ? arg : x)), argsOrig)) as ReturnType<Func>
+/**
+ * Partial function call
+ * 
+ * 
+ * @param func         function
+ * @param bindingArgs  binding args except place holders like _1, _2, _3 ...
+ */
+export function partial<Func extends (...args:any[])=>any, ArgsType extends PartialArgsType<Func>>(func:Func, ...bindingArgs:ArgsType):(...unboundArgs:PartialUnboundArgsType<ArgumentsType<Func>, ArgsType>)=>ReturnType<Func> {
+    return function (...unboundArgs:any[]) {    
+        return func.call(func, ...unboundArgs.reduce((result:any[], arg:any, idx:number)=>result.map((x:any)=>(x === PH_LIST[idx] ? arg : x)), bindingArgs)) as ReturnType<Func>
      }    
+}
+
+
+
+type GetArgsTupledType<Func extends (...args:any[])=>any> = {
+    nop: Func
+    tupled: (arg:ArgumentsType<Func>)=>ReturnType<Func>
+} [ Comp<Length<ArgumentsType<Func>>, 2> extends -1 ? "nop" : "tupled"]
+
+function getArgsTupled<Func extends (...args:any[])=>any>(func:Func, tupled:boolean):GetArgsTupledType<Func> {
+    return ((args:any)=> {
+        if (tupled) {
+            return func.call(func, ...args)
+        }
+        else {
+            return func.call(func, args)
+        }
+    }) as GetArgsTupledType<Func>
+}
+
+function countPH(args:any[]):number {
+    return args.reduce((acc:number, curr:any)=>(PH_LIST.indexOf(curr) >= 0) ? acc + 1 : acc, 0)
+}
+
+type ReaderifiedFuncType<Func extends (...args:any[])=>any, ArgsType extends PartialArgsType<Func>> = GetArgsTupledType<(...unboundArgs:PartialUnboundArgsType<ArgumentsType<Func>, ArgsType>)=>ReturnType<Func>>
+
+/**
+ * Partial function call convenient for Reader monad, create (e:E)=>A
+ * 
+ * If signle arguments is unbound, then it is used as-is for E
+ * If multiple arguments are unbound, then they are packed in a tuple [E1, E2] for E
+ * 
+ * @param func         function chained as Reader monad
+ * @param bindingArgs  binding args except place holders like _1, _2, _3 ...
+ */
+export function readerify<Func extends (...args:any[])=>any, ArgsType extends PartialArgsType<Func>>(func:Func, ...bindingArgs:ArgsType):ReaderifiedFuncType<Func, ArgsType> {
+    const boundFunc = partial(func, ...bindingArgs)
+    return getArgsTupled(boundFunc, countPH(bindingArgs) >= 2)
 }
--- test/partial.test.ts
+++ test/partial.test.ts
@@ -1,5 +1,5 @@
 import * as chai from "chai"
-import { partial, _1, _2, _3, _4 } from "../src/partial"
+import { partial, readerify, _1, _2, _3, _4 } from "../src/partial"
 
 
 describe("boost-ts", ()=>{
@@ -21,5 +21,20 @@ describe("boost-ts", ()=>{
     it("partial mixed", ()=>{        
         const mixed_bound = partial(mixed, _2, 5, true, _1, false)
         chai.assert.equal(mixed_bound("bound", false), "bound")
+    })
+
+    it("readerify bind argument", ()=>{        
+        const sub5 = readerify(sub, _1, 5)
+        chai.assert.equal(sub5(20), 15)
+    })
+
+    it("readerify swap argument", ()=>{        
+        const sub_reversed = readerify(sub, _2, _1)
+        chai.assert.equal(sub_reversed([5, 20]), 15)
+    })
+  
+    it("readerify mixed", ()=>{        
+        const mixed_bound = readerify(mixed, _2, 5, true, _1, false)
+        chai.assert.equal(mixed_bound(["bound", false]), "bound")
     })
 })
